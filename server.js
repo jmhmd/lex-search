81f78a68784afe498e354dfd54806f9cb5af3f76
jmhmd <jhostetter@gmail.com>
2016-10-04 21:32:33 -0400

    Add process file, update port config

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ app.get('/prefetch', function (req, res) {
   return res.send([]);
 })
 
-app.listen(3000, function () {
-  console.log('Lex-search server listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Lex-search server listening on port ${port}');
 });
